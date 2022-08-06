f44623021084f3f694c43d1857d843d9e729a8f9
Gliese185C <104253313+Gliese185C@users.noreply.github.com>
2022-08-06 07:10:14 -0700

    Add files via upload

---
+++ ArrayToZero/Kata/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//my solution
+int[] zero = {1,0,2,0,3,0,4,0};
+int[] new_zer = new int[zero.Length];
+int count = 0;
+for (int i = 0; i < zero.Length; i++)
+{
+    if (zero[i] != 0)
+    {
+        new_zer[count] = zero[i];
+        count++;
+    }
+}
+//expert solution
+zero.OrderBy(x => x==0).ToArray();
